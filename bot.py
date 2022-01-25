fa85bd417a5816bc8820b78708251ce3320abef1
Фетисов Данила <fetisoff.danila2014@yandex.ru>
2022-01-25 18:08:29 +0300

    change

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def send_welcome(message: types.Message):
         "Привет! Я могу перенести стиль с одной картинки на другую. "
         "Пришли мне две фотографии: с первой фотографии я попробую перенсти "
         "стиль на вторую, и отправлю получившееся изображение Вам в ответ. "
-        "Если что, то обработка в среднем занимает не больше 30 секунд."
+        "Если что, то обработка в среднем занимает не больше 30 секунд.  Я сейчас в директории " + str(os.getcwd())
     )
 
 
@@ -62,16 +62,19 @@ async def handle_docs_photo(msg):
         a.clear()
         await bot.send_message(msg.from_user.id, 'Очистил массив')
 
-    download_dir = './img/'
-    if not os.path.exists(download_dir):
-        os.makedirs(download_dir)
-        await bot.send_message(msg.from_user.id, 'Создал папку  ' + str(os.getcwd()))
-    os.chdir(download_dir)
+    # download_dir = './img/'
+    # if not os.path.exists(download_dir):
+    #     os.makedirs(download_dir)
+    #     await bot.send_message(msg.from_user.id, 'Создал папку  ' + str(os.getcwd()))
+    # os.chdir(download_dir)
+
+    '/app'
     await bot.send_message(msg.from_user.id, 'В директории я сейчас  ' + str(os.getcwd()))
+
     img_name = 'img' + str(msg.from_user.id) + str(random_number) + '.jpg'
-    path_to_img = os.getcwd() + '\\img\\' + img_name
+    path_to_img = os.getcwd() + '/' + img_name
     a.append(path_to_img)
-    await msg.photo[-1].download('./img/' + img_name)
+    await msg.photo[-1].download(path_to_img)
     if len(a) == 2:
         await bot.send_message(msg.from_user.id, 'Зашел в обработку')
 
@@ -81,30 +84,21 @@ async def handle_docs_photo(msg):
         out = transfering_style(a)
         await bot.send_message(msg.from_user.id, 'Изобр ' + str(out.shape))
         await bot.send_message(msg.from_user.id, 'Закончил обработку')
-        # end_time_2 = time.time()
 
-        # print('Inference 1st model is:', end_time_1-start_time_1)
-        # print('Inference 2st model is:', end_time_2 - start_time_2)
-
-        # print('begin saving')
-        # print(output.shape)
-        # print(out.shape)
-
-        # output = output.cpu().clone().detach()
         out = out.cpu().clone().detach()
 
         # img1 = output[0]
         # save_image(img1, './img/' + 'saving_photo_1.jpg')
         # path_save_img_1 = './img/' + 'saving_photo_1.jpg'
 
-        img1 = out[0]
-        save_image(img1, './img/' + 'saving_photo_2.jpg')
-        path_save_img_2 = './img/' + 'saving_photo_2.jpg'
+        img = out[0]
+        save_image(img, '/app/' + 'saving_photo.jpg')
+        path_save_img = '/app/' + 'saving_photo.jpg'
 
         # print('end saving')
         # photo(path_save_img)
         # await bot.send_photo(msg.from_user.id, types.InputFile(path_save_img_1))
-        await bot.send_photo(msg.from_user.id, types.InputFile(path_save_img_2))
+        await bot.send_photo(msg.from_user.id, types.InputFile(path_save_img))
         # await msg.answer('Фото прислано')
         await bot.send_message(msg.from_user.id, 'Фото готово и прислано Вам')
         # print('end of working')
@@ -112,10 +106,9 @@ async def handle_docs_photo(msg):
         try:
             os.remove(a[0])
             os.remove(a[1])
-            os.remove('./img/' + 'saving_photo_2.jpg')
+            os.remove('/app/' + 'saving_photo.jpg')
         except:
             FileNotFoundError
-        # print('end removing')
 
 
 @dp.message_handler()

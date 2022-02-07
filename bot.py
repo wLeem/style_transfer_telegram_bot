66471f6d1733ab5e5ee38f3f7b3910f25bcd2eeb
Фетисов Данила <fetisoff.danila2014@yandex.ru>
2022-02-07 16:13:22 +0300

    delete all

--- bot.py
+++ bot.py
@@ -37,18 +37,18 @@ async def send_welcome(message: types.Message):
     )
 
 
-@dp.message_handler(commands=['help'])
-async def send_help(message: types.Message):
-    await message.reply(
-        "Привет! Давайте попробуем разобраться, что у Вас не получается. Вы можете прислать два изображения и бот "
-        "с первой фотографии перенесёт стиль на вторую, после чего отправит получившееся изображение Вам. "
-        "Если что, то обработка в среднем занимает не больше минуты, но может и больше, поэтому если изображение "
-        "не приходит быстро, пожалуйста подождите чуть больше. Также не стоит отправлять новые изображения пока бот не"
-        " обработал предыдущие. Чтобы проверить, что бот работает, Вы можете написать "
-        "любое сообщение и бот пришлет его Вам в ответ."
-    )
-
-
+# @dp.message_handler(commands=['help'])
+# async def send_help(message: types.Message):
+#     await message.reply(
+#         "Привет! Давайте попробуем разобраться, что у Вас не получается. Вы можете прислать два изображения и бот "
+#         "с первой фотографии перенесёт стиль на вторую, после чего отправит получившееся изображение Вам. "
+#         "Если что, то обработка в среднем занимает не больше минуты, но может и больше, поэтому если изображение "
+#         "не приходит быстро, пожалуйста подождите чуть больше. Также не стоит отправлять новые изображения пока бот не"
+#         " обработал предыдущие. Чтобы проверить, что бот работает, Вы можете написать "
+#         "любое сообщение и бот пришлет его Вам в ответ."
+#     )
+#
+#
 @dp.message_handler(commands=['file_in_directory'])
 async def handle_docs_dir(message: types.Message):
     # await message.reply('Я сейчас в директории: ' + os.getcwd())
@@ -58,73 +58,73 @@ async def handle_docs_dir(message: types.Message):
         strk += file
         strk += "; "
     await message.reply('Файлы в рабочей директории :\n ' + strk)
-
-
-a = []
-dct = {}
-
-
-@dp.message_handler(commands=['clean'])
-def clean(message: types.Message):
-    global a
-    a = []
-    await bot.send_message(message.from_user.id, 'Зашел в функцию по удалению элементов')
-    files = os.listdir(os.getcwd())
-    for file in files:
-        if ".jpg" or ".png" in file:
-            await bot.send_message(message.from_user.id, 'Зашел в цикл в функции по удалению элементов и удалил ' +
-                                   str('/app/' + file))
-            os.remove('/app/' + file)
-
-
-@dp.message_handler(content_types=['photo'])
-async def handle_docs_photo(msg):
-    if len(a) >= 2:
-        clean()
-        await bot.send_message(msg.from_user.id, 'Очистил массив')
-
-    # '/app'
-    await bot.send_message(msg.from_user.id, 'Я сейчас в директории ' + str(os.getcwd()))
-
-    img_name = 'img' + '_' + str(len(a)) + '.jpg'
-    path_to_img = os.getcwd() + '/' + img_name
-    a.append(path_to_img)
-    if len(a) == 1:
-        await bot.send_message(msg.from_user.id, 'Фото получил, с него будем переносить стиль. Жду второе фото!')
-    await msg.photo[-1].download(path_to_img)
-    # await bot.send_message(msg.from_user.id, 'Длина массива сейчас ' + str(len(a)))
-    if len(a) == 2:
-        await bot.send_message(msg.from_user.id, 'Второе фото получено, начинаю обработку, '
-                                                 'пожалуйста подождите чуть-чуть!')
-        # await bot.send_message(msg.from_user.id, 'Зашел в обработку')
-        # await bot.send_message(msg.from_user.id, 'Начал обработку')
-        # await bot.send_message(msg.from_user.id, 'Длина массива в цикле при переносе ' + str(len(a)))
-        # await bot.send_message(msg.from_user.id, 'Первый элемент ' + a[0])
-        # await bot.send_message(msg.from_user.id, 'Второй элемент  ' + a[1])
-        start_time = time.time()
-        out = transfering_style(a)
-        end_time = time.time()
-        # await bot.send_message(msg.from_user.id, 'Изобр ' + str(out.shape))
-        await bot.send_message(msg.from_user.id, 'Закончил обработку, сейчас пришлю получившееся изображение')
-        await bot.send_message(msg.from_user.id, 'Обработка заняла: ' + str(end_time - start_time))
-
-        out = out.cpu().clone().detach()
-
-        img = out[0]
-        path_save_img = '/app/' + 'saving_photo.jpg'
-        save_image(img, path_save_img)
-
-        await bot.send_photo(msg.from_user.id, types.InputFile(path_save_img))
-        await bot.send_message(msg.from_user.id, 'Фото готово и прислано Вам')
-        clean()
-        # await bot.send_message(msg.from_user.id, 'Удалил все файлы')
-        try:
-            os.remove(a[0])
-            os.remove(a[1])
-            os.remove('/app/' + 'saving_photo.jpg')
-            await bot.send_message(msg.from_user.id, 'Удалил все файлы')
-        except FileNotFoundError:
-            pass
+#
+#
+# a = []
+# dct = {}
+#
+#
+# @dp.message_handler(commands=['clean'])
+# def clean(message: types.Message):
+#     global a
+#     a = []
+#     await bot.send_message(message.from_user.id, 'Зашел в функцию по удалению элементов')
+#     files = os.listdir(os.getcwd())
+#     for file in files:
+#         if ".jpg" or ".png" in file:
+#             await bot.send_message(message.from_user.id, 'Зашел в цикл в функции по удалению элементов и удалил ' +
+#                                    str('/app/' + file))
+#             os.remove('/app/' + file)
+#
+#
+# @dp.message_handler(content_types=['photo'])
+# async def handle_docs_photo(msg):
+#     if len(a) >= 2:
+#         clean()
+#         await bot.send_message(msg.from_user.id, 'Очистил массив')
+#
+#     # '/app'
+#     await bot.send_message(msg.from_user.id, 'Я сейчас в директории ' + str(os.getcwd()))
+#
+#     img_name = 'img' + '_' + str(len(a)) + '.jpg'
+#     path_to_img = os.getcwd() + '/' + img_name
+#     a.append(path_to_img)
+#     if len(a) == 1:
+#         await bot.send_message(msg.from_user.id, 'Фото получил, с него будем переносить стиль. Жду второе фото!')
+#     await msg.photo[-1].download(path_to_img)
+#     # await bot.send_message(msg.from_user.id, 'Длина массива сейчас ' + str(len(a)))
+#     if len(a) == 2:
+#         await bot.send_message(msg.from_user.id, 'Второе фото получено, начинаю обработку, '
+#                                                  'пожалуйста подождите чуть-чуть!')
+#         # await bot.send_message(msg.from_user.id, 'Зашел в обработку')
+#         # await bot.send_message(msg.from_user.id, 'Начал обработку')
+#         # await bot.send_message(msg.from_user.id, 'Длина массива в цикле при переносе ' + str(len(a)))
+#         # await bot.send_message(msg.from_user.id, 'Первый элемент ' + a[0])
+#         # await bot.send_message(msg.from_user.id, 'Второй элемент  ' + a[1])
+#         start_time = time.time()
+#         out = transfering_style(a)
+#         end_time = time.time()
+#         # await bot.send_message(msg.from_user.id, 'Изобр ' + str(out.shape))
+#         await bot.send_message(msg.from_user.id, 'Закончил обработку, сейчас пришлю получившееся изображение')
+#         await bot.send_message(msg.from_user.id, 'Обработка заняла: ' + str(end_time - start_time))
+#
+#         out = out.cpu().clone().detach()
+#
+#         img = out[0]
+#         path_save_img = '/app/' + 'saving_photo.jpg'
+#         save_image(img, path_save_img)
+#
+#         await bot.send_photo(msg.from_user.id, types.InputFile(path_save_img))
+#         await bot.send_message(msg.from_user.id, 'Фото готово и прислано Вам')
+#         clean()
+#         # await bot.send_message(msg.from_user.id, 'Удалил все файлы')
+#         try:
+#             os.remove(a[0])
+#             os.remove(a[1])
+#             os.remove('/app/' + 'saving_photo.jpg')
+#             await bot.send_message(msg.from_user.id, 'Удалил все файлы')
+#         except FileNotFoundError:
+#             pass
 
 
 @dp.message_handler()
